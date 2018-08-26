606b8f6447c1864d70eb17acf5354dcdf5316105
Sampuccino <Sampuccino@gmail.com>
2018-08-25 22:50:51 -0700

    Fixed some UI issues and started validation process for form.

--- Index.php
+++ Index.php
@@ -2,45 +2,49 @@
 
 <div class="container centerer animated fadeIn" id="form-container" style="margin-top: 3rem !important;">
 
-  <div class="row" style="height: 100vh !important;">
+  <div class="row" id="form-container-row">
 
     <div class="col-12 container-border"></div>
 
     <div class="col-md-6 col-10 mx-auto text-center">
-      <h5 id="welcome-message"><b>Welcome</b> To Email Buddy</h5>
+      <h5 class="mt-4" id="welcome-message"><b>Welcome</b> To Email Buddy</h5>
       <div class="alert alert-warning mt-md-4 mt-4 Work-Sans-Semi-Bold">Try Sending An Email To A Friend.</div>
     </div>
 
     <div class="col-12 p-md-5 p-3 m-md-3 m-sm-0" id="content-container">
       <form>
         <div class="form-group">
-          <label for="exampleFormControlInput1">Email address</label>
-          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+          <label for="email">Email address</label>
+          <input  type="email" class="form-control" id="email" placeholder="name@example.com">
         </div>
         <div class="form-group">
-          <label for="exampleFormControlInput1">Email address</label>
-          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+          <label for="subject">Subject</label>
+          <input type="text" class="form-control" id="subject" placeholder="A funny witty title">
         </div>
         <div class="form-group">
-          <label for="exampleFormControlTextarea1">Example textarea</label>
-          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          <label for="message">Mesasge</label>
+          <textarea class="form-control" id="message" placeholder="Maybe how you're day has been going?" rows="3"></textarea>
         </div>
       </form>
     </div> <!--#content-container-->
 
+    <div class="col-12 container-border"></div>
+
     <!-- Bottom of form container ( 3 buttons) -->
     <div class="col-12 text-center p-4 mx-auto" id="form-bottom-container">
       <div class="row">
-        <div class="col-md-4 col-12 form-bottom-button my-auto">
+        <!-- Send Button -->
+        <button class="col-md-4 col-12 btn btn-light form-bottom-button my-auto" id="send-button">
           <img src="resources/img/149063.svg" width="30" alt="">
           <br>
           <small class="Work-Sans-Light">Send</small>
-        </div>
-        <div class="col-md-4 col-12 form-bottom-button my-auto">
+        </button>
+        <div class="col-md-4 col-12 form-bottom-button my-auto" >
           <img src="resources/img/buddy.svg" width="30" alt="">
           <br>
           <small class="Work-Sans-Semi-Bold">Email Buddy</small>
         </div>
+        <!-- Reset Button -->
         <div class="col-md-4 col-12 form-bottom-button my-auto">
           <img src="resources/img/151956.svg"  width="30" alt="">
           <br>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,30 @@
 console.log('Script loaded in!');
+
+/* Variables */
+const sendButton = $('#send-button'), email = $('#email'), subject = $('#subject'), message = $('#message');
+
+eventListeners();
+
+function eventListeners() {
+
+  document.addEventListener('DOMContentLoaded', appInit);
+
+  email.addEventListener('blur', validateField);
+  subject.addEventListener('blur', validateField);
+  message.addEventListener('blur', validateField);
+
+  // Checking to make sure button is disabled
+  sendButton.click(function () {
+    console.log("Clicked");
+  });
+}
+
+function appInit() {
+  //Disable button when initializing app
+  sendButton.prop("disabled", true);
+
+}
+
+function validateField() {
+  
+}
